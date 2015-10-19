a946394ff180167653b4c07a8ffde8d258e4b238
Diana Gospodarets <dgospodarets@gmail.com>
2015-10-19 16:29:55 +0100

    filling images page by page

--- js/pictures.js
+++ js/pictures.js
@@ -7,11 +7,30 @@
    * global vars for this file
    */
   var picturesData = [];
+  var picturesDataFiltered = [];
+  var PICTURES_ON_PAGE = 12;
+  var currentPage = 0;
+
+  /**
+   * Fills next page by images
+   * and tracks current page index
+   */
+  function fillNextPage(){
+    var picturesFrom = currentPage * PICTURES_ON_PAGE;
+    var picturesTo = picturesFrom + PICTURES_ON_PAGE;
+
+    // check the last page
+    if (picturesFrom > picturesData.length) return;
+
+    fillImages(picturesDataFiltered, picturesFrom, picturesTo);
+
+    currentPage = currentPage + 1;
+  }
 
   /**
    * Insert image
    * @param template {{DomeNode}}
-   * @param pictureUrl {{String}}
+   * @param pictureUrl {String}
    */
   function insertImage(template, pictureUrl) {
     var image = new Image(182, 182);
@@ -30,11 +49,9 @@
   /**
    * Function to be colled
    * when pictures data is loaded
-   *
-   * @param pictures {{Array}}
    */
-  function onAjaxSuccess(pictures) {
-    fillImages(pictures);
+  function onAjaxSuccess() {
+    fillNextPage();
 
     document.querySelector('.filters').classList.remove('hidden');//show filters block
 
@@ -44,16 +61,20 @@
   }
 
   /**
-   * Removes pictures and fills by new ones
+   * Adds pictures
    *
-   * @param pictures {{Array}}
+   * @param pictures {Array}
+   * @param startIndex {Number}
+   * @param [endIndex] {Number}
    */
-  function fillImages(pictures) {
+  function fillImages(pictures, startIndex, endIndex) {
     // VARS
     var fragment = document.createDocumentFragment();
     var picturesEl = document.querySelector('.pictures');
 
-    //PROCESS EACH PICTURE
+    //PROCESS PAGE PICTURES
+    endIndex = endIndex || startIndex + PICTURES_ON_PAGE;
+    pictures = pictures.slice(startIndex, endIndex);
     pictures.forEach(function (picture) {
       var template = document.querySelector('.picture-template').cloneNode(true);
 
@@ -64,9 +85,25 @@
       fragment.appendChild(template.content);
     });
 
-    // REMOVE AND FILL
-    picturesEl.innerHTML = '';
+    // FILL
     picturesEl.appendChild(fragment);
+  }
+
+  /**
+   * Removes pictures and fills by new ones
+   *
+   * @param pictures {Array}
+   * @param startIndex {Number}
+   * @param [endIndex] {Number}
+   */
+  function removeAndFillImages(pictures, startIndex, endIndex) {
+    // VARS
+    var picturesEl = document.querySelector('.pictures');
+
+    // REMOVE
+    picturesEl.innerHTML = '';
+
+    fillImages(pictures, startIndex, endIndex);
   }
 
   /**
@@ -80,7 +117,7 @@
   function onFiltersClick(event) {
     // VARS
     var target = event.target;
-    var picturesDataCopy = picturesData.slice();
+    picturesDataFiltered = picturesData.slice();
 
     // check which filter was clicked
     switch (target.getAttribute('value')) {
@@ -88,6 +125,7 @@
      * POPULAR
      */
       case 'popular':
+        picturesDataFiltered = picturesData;
         break;
 
     /**
@@ -95,7 +133,7 @@
      */
       case 'new':
         // filter
-        picturesDataCopy = picturesDataCopy.filter(function (pictureData) {
+        picturesDataFiltered = picturesDataFiltered.filter(function (pictureData) {
           var today = new Date;
           var oneMonthAgo = +new Date(today.getUTCFullYear(), today.getMonth() - 1, today.getDate());
 
@@ -110,8 +148,8 @@
         });
 
         // sort
-        picturesDataCopy.sort(function (a, b) {
-          return a.date > b.date;// ToDo doesn't work as expected
+        picturesDataFiltered.sort(function (a, b) {
+          return +new Date(b.date) - +new Date(a.date);
         });
 
         break;
@@ -121,15 +159,15 @@
      */
       case 'discussed':
         // sort
-        picturesDataCopy.sort(function (a, b) {
-          return a.comments < b.comments;// ToDo doesn't work as expected
+        picturesDataFiltered.sort(function (a, b) {
+          return b.comments - a.comments;
         });
     }
 
     /**
      * Run a refill of pictures block
      */
-    fillImages(picturesDataCopy);
+    removeAndFillImages(picturesDataFiltered, 0);
   }
 
   /**
@@ -165,6 +203,8 @@
       if (xhr.status === 200) {
         // XHR SUCCESS
         picturesData = JSON.parse(xhr.responseText);
+        picturesDataFiltered = picturesData;
+
         onAjaxSuccess(picturesData);
       }
     }
@@ -182,5 +222,4 @@
    * XHR SEND
    */
   xhr.send();
-
 }());
